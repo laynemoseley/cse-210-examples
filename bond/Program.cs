acf91e9015afaf06f6f7f867bf3fd2881ddca04b
Layne Moseley <laynemoseley@gmail.com>
2022-09-20 11:04:02 -0600

    bond

---
+++ bond/Program.cs
@@ -0,0 +1,1 @@
+// Convert bond.py into C# code
---
+++ bond/bond.py
@@ -0,0 +1,4 @@
+first = input("What is your first name? ")
+last = input("What is your last name? ")
+
+print(f"Your name is {last}, {first} {last}.")
