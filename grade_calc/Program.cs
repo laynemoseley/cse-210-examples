ba02b3761186320f7a88c83fd68dbd2a952654b6
Layne Moseley <laynemoseley@gmail.com>
2022-09-20 13:02:46 -0600

    stuff

---
+++ grade_calc/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("What is your grade percent?");
+string? grade_string = Console.ReadLine();
+int grade = Int32.Parse(grade_string ?? "0");
+string letter_grade = "F";
+
+if (grade >= 90) {
+    letter_grade = "A";
+} else if (grade >= 80) {
+    letter_grade = "B";
+} else if (grade >= 70) {
+    letter_grade = "C";
+} else if (grade >= 60) {
+    letter_grade = "D";
+}
+
+Console.WriteLine($"Your letter grade is {letter_grade}");
+
+if (letter_grade == "A" || letter_grade == "B" || letter_grade == "C") {
+    Console.WriteLine("Well done, you passed!");
+} else {
+    Console.WriteLine("FAILURE");
+}
---
+++ grade_calc/grade.py
@@ -0,0 +1,19 @@
+grade = int(input("What is your grade percent? "))
+
+if grade >= 90:
+    letter = "A"
+elif grade >= 80:
+    letter = "B"
+elif grade >= 70:
+    letter = "C"
+elif grade >= 60:
+    letter = "D"
+else:
+    letter = "F"
+
+print(f"Your letter grade is: {letter}")
+
+if grade >= 70:
+    print("Congratulations! You passed the class!")
+else:
+    print("Stay focused and you'll get it next time!")
