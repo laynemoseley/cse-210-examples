c76c2e789e5b9e6547e6180e67a5d545910ad901
Layne Moseley <laynemoseley@gmail.com>
2022-09-21 09:19:05 -0600

    ok

---
+++ hello_world_section_7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter your first name:");
+string? first = Console.ReadLine();
+Console.WriteLine("Enter your last name:");
+string? last = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last}, {first} {last}.");
